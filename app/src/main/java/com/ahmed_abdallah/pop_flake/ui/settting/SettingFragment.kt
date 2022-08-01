2acd36633ee4b6e39f2dc229a85c8054b30a8325
AhmedMohamedAbdallah <57001378+AhmedAbdallah125@users.noreply.github.com>
2022-08-01 15:00:17 +0200

    make structure ui project

--- app/src/main/java/com/ahmed_abdallah/pop_flake/ui/settting/SettingFragment.kt
+++ app/src/main/java/com/ahmed_abdallah/pop_flake/ui/settting/SettingFragment.kt
@@ -1,4 +1,4 @@
-package com.ahmed_abdallah.pop_flake.ui.dashboard
+package com.ahmed_abdallah.pop_flake.ui.settting
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.ahmed_abdallah.pop_flake.databinding.FragmentDashboardBinding
+import com.ahmed_abdallah.pop_flake.databinding.FragmentSettingBinding
 
-class DashboardFragment : Fragment() {
+class SettingFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentSettingBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val settingViewModel =
+            ViewModelProvider(this).get(SettingViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textNotifications
+        settingViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
