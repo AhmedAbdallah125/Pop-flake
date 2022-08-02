30146f3e9fbfb7c8ce5ce2d16a704b8a633ee0e2
AhmedMohamedAbdallah <57001378+AhmedAbdallah125@users.noreply.github.com>
2022-08-02 17:42:02 +0200

    make retrofit calls

--- app/src/main/java/com/ahmed_abdallah/pop_flake/model/data/remoteSource/RetrofitServices.kt
+++ app/src/main/java/com/ahmed_abdallah/pop_flake/model/data/remoteSource/RetrofitServices.kt
@@ -1,6 +1,8 @@
 package com.ahmed_abdallah.pop_flake.model.data.remoteSource
 
+import com.ahmed_abdallah.pop_flake.pojo.BoxOfficeMovie
 import com.ahmed_abdallah.pop_flake.pojo.MovieAPI
+import com.ahmed_abdallah.pop_flake.pojo.TopRatedMovie
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -13,4 +15,18 @@ interface RetrofitServices {
     suspend fun getInTheatresMovies(
         @Query("apiKey") apiKey: String = APIKEY
     ): Response<MovieAPI>
+
+    @GET("ComingSoon")
+    suspend fun getComingSoonMovies(
+        @Query("apiKey") apiKey: String = APIKEY
+    ): Response<MovieAPI>
+
+    @GET("Top250Movies")
+    suspend fun getTop250Movies(
+        @Query("apiKey") apiKey: String = APIKEY
+    ): Response<TopRatedMovie>
+    @GET("BoxOffice")
+    suspend fun getBoxOfficeMovies(
+        @Query("apiKey") apiKey: String = APIKEY
+    ): Response<BoxOfficeMovie>
 }
